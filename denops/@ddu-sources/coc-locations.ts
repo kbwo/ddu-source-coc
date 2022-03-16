a5413e8d512f0573d207ce68abc19ea8b88f6893
Kodai Kabasawa <kabaaa1126@gmail.com>
2022-03-16 16:08:06 +0900

    display location path

--- denops/@ddu-sources/coc-locations.ts
+++ denops/@ddu-sources/coc-locations.ts
@@ -7,6 +7,7 @@ import {
 import { Denops } from "https://deno.land/x/ddu_vim@v1.2.0/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.2.0/file.ts";
 import { globals } from "https://deno.land/x/denops_std@v3.1.4/variable/variable.ts";
+import * as fn from "https://deno.land/x/denops_std@v3.1.4/function/mod.ts";
 
 type Params = Record<never, never>;
 
@@ -46,17 +47,22 @@ export class Source extends BaseSource<Params> {
       async start(controller) {
         const cocJumpLocations: LocationData[] =
           await globals.get(denops, "coc_jump_locations") ?? [];
-        let items: Item<ActionData>[] = [];
-        items = cocJumpLocations.map((location) => ({
-          word: location.text,
-          action: {
-            path: location.filename,
-            col: location.col,
-            bufNr: location.bufnr,
-            text: location.text,
-            lineNr: location.lnum,
-          },
-        }));
+        const currentWorkingDir = await fn.getcwd(denops) as string;
+        const items: Item<ActionData>[] = cocJumpLocations.map((l) => {
+          const pathText = l.filename.includes(currentWorkingDir)
+            ? l.filename.replace(currentWorkingDir, "")
+            : l.filename;
+          return {
+            word: pathText + "   " + l.text,
+            action: {
+              path: l.filename,
+              col: l.col,
+              bufNr: l.bufnr,
+              text: l.text,
+              lineNr: l.lnum,
+            },
+          };
+        });
         if (items.length) {
           controller.enqueue(items);
         }
